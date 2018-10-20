5782db0da2ec7273615c6466a38c377f9fbba340
rfairley <rob.t.fairley@gmail.com>
2018-10-20 12:10:59 -0400

    check every second

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ var Cloudant = require('@cloudant/cloudant');
 var async_req = require('async');
 
 const port = 3001
-const url = '';
+const url = 'https://dminsitchappordediverede:0d213f72e3dda91a6d27acd1bb23d4243a3f5d45@958edf45-e095-4a52-99d1-ce088c9b2c39-bluemix.cloudant.com';
 const app = express()
 var cloudant = Cloudant({url: url});
 const dbname = 'sensordata';
@@ -22,43 +22,32 @@ if (!url) {
   process.exit(1);
 }
 
+var time = 0;
+var average = 0;
 db = cloudant.db.use(dbname);
 
-// // read a document
-// var readDocument = function(callback) {
-//   console.log("Reading document '2309ee5cd716d9b951580276fec3d940'");
-//   db.get('2309ee5cd716d9b951580276fec3d940', function(err, data) {
-//     console.log('Error:', err);
-//     console.log('Data:', data);
-//     // keep a copy of the doc so we know its revision token
-//     doc = data;
-//     callback(err, data);
-//   });
-// };
+setInterval(() => {
+    console.log('Checking db for new data...');
+    db.find({
+       "selector": {
+         "payload": {
+             "d": {
+               "time": {
+                 "$gt": time
+               }
+             }
+         }
+       }
+    }, function(er, result) {
+      if (er) {
+        throw er;
+      }
 
-db.index(function(er, result) {
-  if (er) {
-    throw er;
-  }
-
-  console.log('The database has %d indexes', result.indexes.length);
-  for (var i = 0; i < result.indexes.length; i++) {
-    console.log('  %s (%s): %j', result.indexes[i].name, result.indexes[i].type, result.indexes[i].def);
-  }
-});
-
-db.find({
-   "selector": {
-   }
-}, function(er, result) {
-  if (er) {
-    throw er;
-  }
-
-  console.log('Found 1 documents with name Alice', result.docs.length);
-  for (var i = 0; i < result.docs.length; i++) {
-    console.log('  Doc id: %s, time: ', result.docs[i]._id, result.docs[i].payload);
-  }
-});
-//
-// async_req.series([readDocument]);
+      for (var i = 0; i < result.docs.length; i++) {
+        console.log('Doc id: %s, payload: ', result.docs[i]._id, result.docs[i].payload);
+        if (result.docs[i].payload.d.time > time) {
+          time = result.docs[i].payload.d.time;
+        }
+      }
+    });
+}, 1000)
