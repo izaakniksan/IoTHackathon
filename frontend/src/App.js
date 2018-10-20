93d3e1e912a0cb874f3a16060c9d94cbc2c0d567
Musa Talluzi <musa.talluzi@gmail.com>
2018-10-20 05:56:23 -0400

    Plot data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { DATA } from './data';
+import '../node_modules/react-vis/dist/style.css';
+import {
+  XYPlot, XAxis, YAxis, VerticalGridLines,
+  HorizontalGridLines, LineMarkSeries
+} from 'react-vis';
 
 class App extends Component {
+
   render() {
+    var data = [[], [], [], []];
+    console.log(DATA);
+    for (var i = 0; i < DATA.length; i++) {
+      data[0].push({ x: i, y: DATA[i].d.Temp });
+      data[1].push({ x: i, y: DATA[i].d.Pressure / 10 })
+      data[2].push({ x: i, y: DATA[i].d.Humidity });
+      data[3].push({ x: i, y: DATA[i].d.Gas });
+    }
+
     return (
-      <div></div>
+      <div className="App">
+        <XYPlot height={600} width={600}>
+          <VerticalGridLines />
+          <HorizontalGridLines />
+          <XAxis />
+          <YAxis />
+          <LineMarkSeries
+            className="tempSeries"
+            style={{strokeWidth: '3px'}}
+            data={data[0]} />
+          <LineMarkSeries className="pressureSeries" data={data[1]} />
+          <LineMarkSeries className="humiditySeries" data={data[2]} />
+          <LineMarkSeries className="gasSeries" data={data[3]} />
+        </XYPlot>
+      </div>
     );
+
   }
 }
 
