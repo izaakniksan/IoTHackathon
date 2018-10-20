84b1aa5cc46fd20e6f85fa118b176fb10f482fcd
Musa Talluzi <musa.talluzi@gmail.com>
2018-10-20 13:25:45 -0400

    Get new data every second

--- backend/index.js
+++ backend/index.js
@@ -5,15 +5,69 @@ var Cloudant = require('@cloudant/cloudant');
 var async_req = require('async');
 
 const port = 3001
-const url = '';
+const url = 'https://dminsitchappordediverede:0d213f72e3dda91a6d27acd1bb23d4243a3f5d45@958edf45-e095-4a52-99d1-ce088c9b2c39-bluemix.cloudant.com';
 const app = express()
-var cloudant = Cloudant({url: url});
+var cloudant = Cloudant({ url: url });
 const dbname = 'sensordata';
 var db = null;
-var doc = null;
+var time = 0;
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.get('/', (req, res) => res.send('Hello World node!'))
-app.get('/data', (req, res) => res.send("data here" + JSON.stringify(doc)))
+app.get('/data', (req, res) => {
+  db.find({
+    "selector": {
+    }
+  }, function (er, result) {
+    if (er) {
+      console.log(err);
+      res.status = 401;
+      res.send('Error');
+    }
+    console.log('Found 1 documents of length', result.docs.length);
+    console.log('  Doc id: %s, payload: ', result.docs[0]._id, result.docs[0].payload);
+
+    res.status = 200;
+    res.send(JSON.stringify(result.docs));
+  });
+});
+
+app.get('/recentData', (req, res) => {
+  console.log('Checking db for new data...');
+
+  db.find({
+    "selector": {
+      "payload": {
+        "d": {
+          "time": {
+            "$gt": time
+          }
+        }
+      }
+    }
+  }, function (er, result) {
+    if (er) {
+      console.log(err);
+      res.status = 401;
+      res.send('Error');
+    }
+    console.log('Found documents of length', result.docs.length);
+    //  for (var i = 0; i < result.docs.length; i++) {
+    if (result.docs.length > 0) {
+      console.log('  Doc id: %s, payload: ', result.docs[0]._id, result.docs[0].payload);
+      //  }
+      time = result.docs[result.docs.length-1].payload.d.time;
+      console.log('Time: ', time);
+    }
+
+    res.status = 200;
+    res.send(JSON.stringify(result.docs));
+  });
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
@@ -23,42 +77,3 @@ if (!url) {
 }
 
 db = cloudant.db.use(dbname);
-
-// // read a document
-// var readDocument = function(callback) {
-//   console.log("Reading document '2309ee5cd716d9b951580276fec3d940'");
-//   db.get('2309ee5cd716d9b951580276fec3d940', function(err, data) {
-//     console.log('Error:', err);
-//     console.log('Data:', data);
-//     // keep a copy of the doc so we know its revision token
-//     doc = data;
-//     callback(err, data);
-//   });
-// };
-
-db.index(function(er, result) {
-  if (er) {
-    throw er;
-  }
-
-  console.log('The database has %d indexes', result.indexes.length);
-  for (var i = 0; i < result.indexes.length; i++) {
-    console.log('  %s (%s): %j', result.indexes[i].name, result.indexes[i].type, result.indexes[i].def);
-  }
-});
-
-db.find({
-   "selector": {
-   }
-}, function(er, result) {
-  if (er) {
-    throw er;
-  }
-
-  console.log('Found 1 documents with name Alice', result.docs.length);
-  for (var i = 0; i < result.docs.length; i++) {
-    console.log('  Doc id: %s, time: ', result.docs[i]._id, result.docs[i].payload);
-  }
-});
-//
-// async_req.series([readDocument]);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import { DATA } from './data';
 import '../node_modules/react-vis/dist/style.css';
-import Cloudant from '@cloudant/cloudant';
 
 import Plot from 'react-plotly.js';
-
+const dataUrl = 'http://localhost:3001/data';
+const recentDataUrl = 'http://localhost:3001/recentData';
 class App extends Component {
 
   constructor(props) {
@@ -16,68 +15,115 @@ class App extends Component {
       frames: [],
       config: {}
     };
+    this.getItems = this.getItems.bind(this);
   }
 
   componentDidMount() {
-    // let headers = new Headers();
-    // headers.append('Authorization', 'Basic ' + username + ":" + password));
-    // fetch(dburl, {
-    //   headers: headers
-    // })
-    // .then(res => {
-    //   if (res.status == 200) {
-    //     console.log(res.json);
-    //   }
-    // })
-    // .catch(err => console.log(err));
+    console.log('fetching');
+    fetch(dataUrl, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      credentials: "same-origin"
+    })
+      .then(res => {
+        if (res.status === 200) {
+          return res
+        }
+      })
+      .then(res => res.json())
+      .then(res => {
+        var plotData = [[], [], [], [], []];
+        for (var i = 0; i < res.length; i++) {
+          plotData[0].push(res[i].payload.d.time);
+          plotData[1].push(res[i].payload.d.Temp);
+          plotData[2].push(res[i].payload.d.Pressure);
+          plotData[3].push(res[i].payload.d.Humidity);
+          plotData[4].push(res[i].payload.d.Gas);
+        }
+        var updatedData = [
+          {
+            x: plotData[0],
+            y: plotData[1],
+            type: 'scatter',
+            mode: 'lines+points',
+            marker: { color: 'red' },
+            name: 'Temp'
+          },
+          {
+            x: plotData[0],
+            y: plotData[2],
+            type: 'scatter',
+            mode: 'lines+points',
+            marker: { color: 'blue' },
+            name: 'Pressure'
+          },
+          {
+            x: plotData[0],
+            y: plotData[3],
+            type: 'scatter',
+            mode: 'lines+points',
+            marker: { color: 'green' },
+            name: 'Humidity'
+          },
+          {
+            x: plotData[0],
+            y: plotData[4],
+            type: 'scatter',
+            mode: 'lines+points',
+            marker: { color: 'yellow' },
+            name: 'Gas'
+          }
+        ];
+        console.log(plotData);
+        console.log(this.state.data);
 
-    var plotData = [[], [], [], [], []];
-    console.log(DATA);
-    for (var i = 0; i < DATA.length; i++) {
-      plotData[0].push(i);
-      plotData[1].push(DATA[i].d.Temp);
-      plotData[2].push(DATA[i].d.Pressure);
-      plotData[3].push(DATA[i].d.Humidity);
-      plotData[4].push(DATA[i].d.Gas);
-    }
-    var updatedData = [
-      {
-        x: plotData[0],
-        y: plotData[1],
-        type: 'scatter',
-        mode: 'lines+points',
-        marker: { color: 'red' },
-        name: 'Temp'
-      },
-      {
-        x: plotData[0],
-        y: plotData[2],
-        type: 'scatter',
-        mode: 'lines+points',
-        marker: { color: 'blue' },
-        name: 'Pressure'
-      },
-      {
-        x: plotData[0],
-        y: plotData[3],
-        type: 'scatter',
-        mode: 'lines+points',
-        marker: { color: 'green' },
-        name: 'Humidity'
-      },
-      {
-        x: plotData[0],
-        y: plotData[4],
-        type: 'scatter',
-        mode: 'lines+points',
-        marker: { color: 'yellow' },
-        name: 'Gas'
-      }
-    ];
-    console.log(plotData);
-    console.log(this.state.data);
+        this.setState({ data: this.state.data.concat(updatedData) });
+      })
+      .catch(err => console.log(err));
 
-    this.setState({ data: updatedData });
+    this.timer = setInterval(() => this.getItems(), 1000);
+  }
+
+  getItems() {
+    fetch(recentDataUrl, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      credentials: "same-origin"
+    })
+      .then(res => {
+        if (res.status === 200) {
+          return res
+        }
+      })
+      .then(res => res.json())
+      .then(res => {
+        var plotData = [[], [], [], [], []];
+        for (var i = 0; i < res.length; i++) {
+          plotData[0].push(res[i].payload.d.time);
+          plotData[1].push(res[i].payload.d.Temp);
+          plotData[2].push(res[i].payload.d.Pressure);
+          plotData[3].push(res[i].payload.d.Humidity);
+          plotData[4].push(res[i].payload.d.Gas);
+        }
+        var concatData = this.state.data;
+        console.log('data: ', this.state.data[0].x.length);
+        concatData[0].x = concatData[0].x.concat(plotData[0]);
+        concatData[0].y = concatData[0].y.concat(plotData[1]);
+        concatData[1].x = concatData[1].x.concat(plotData[0]);
+        concatData[1].y = concatData[1].y.concat(plotData[2]);
+        concatData[2].x = concatData[2].x.concat(plotData[0]);
+        concatData[2].y = concatData[2].y.concat(plotData[3]);
+        concatData[3].x = concatData[3].x.concat(plotData[0]);
+        concatData[3].y = concatData[3].y.concat(plotData[4]);
+        this.setState({data: concatData});
+        console.log('data: ', this.state.data[0].x.length);
+        this.forceUpdate();
+      })
+      .catch(err => console.log(err));
   }
 
 
